c6802945262361664353977d63db574f56e96dc1
takasek <takassekiyoshi@gmail.com>
2017-12-03 14:05:19 +0900

    Add Xcode Project

---
+++ XCJumpToTests/XCJumpToTests/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  XCJumpToTests
+//
+//  Created by Yoshitaka Seki on 2017/12/03.
+//  Copyright © 2017年 takasek. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
